d86922d6e57e97a74cd132d692946b78d952c30f
Anshul Singh Mehra <123886922+Anshulsm12@users.noreply.github.com>
2025-04-03 12:58:06 +0530

    Update store.js

--- store/store.js
+++ store/store.js
@@ -4,17 +4,14 @@ import cartReducer from './cartSlice';
 import storage from 'redux-persist/lib/storage';
 import { persistReducer, persistStore } from 'redux-persist';
 
-// Configure persist options for cart
 const cartPersistConfig = {
   key: 'cart',
   storage,
   whitelist: ['items']
 };
 
-// Create persisted cart reducer
 const persistedCartReducer = persistReducer(cartPersistConfig, cartReducer);
 
-// Configure store
 export const store = configureStore({
   reducer: {
     products: productReducer,
@@ -29,5 +26,4 @@ export const store = configureStore({
   devTools: process.env.NODE_ENV !== 'production',
 });
 
-// Create persisted store
 export const persistor = persistStore(store);
